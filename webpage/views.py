4c7be65aec4f958d75636dda02f99a8f29b052d3
dbw1 <dbw401@gmail.com>
2016-04-25 12:10:53 -0700

    Changed home.html to accomodate a form template --> home.html has a raw html form written into it, not good for security reasons

--- webpage/views.py
+++ webpage/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+	title = "Hello there %s" %(request.user)
+	context = {
+		"template_title": title
+	}
+	return render(request, "home.html", context)
