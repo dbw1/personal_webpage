6dbe755ea62a6d239181549cbe75a3db5cf51637
dbw1 <dbw401@gmail.com>
2016-04-25 15:30:18 -0700

    Modified home.html to add csrf token and make format prettier Modified views.py home view to incorporate form validation

--- webpage/views.py
+++ webpage/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 
+from .forms import SignUpForm
+
 # Create your views here.
 def home(request):
-	title = "Hello there %s" %(request.user)
+	title = "Hello there"
+	
+	#if request.method == "POST":
+		#print (request.POST)
+	print (request)
+
+	form = SignUpForm(request.POST or None) #if post data send through form or none
+	if form.is_valid():
+		instance = form.save(commit=False)
+		if not instance.full_name:
+			instance.full_name = "NA"
+		instance.save() #saves data
+		print (instance.email)
+	
+	instance = form.save(commit=False)
+	print (instance.full_name)
+	
 	context = {
-		"template_title": title
+		"template_title": title,
+		"form": form,
 	}
 	return render(request, "home.html", context)
